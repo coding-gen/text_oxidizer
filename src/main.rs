17614204ef2a4442ec6cfec2930f9254ed95115e
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-05-26 19:32:43 -0700

    Basic file BufReader, no error checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+
+//https://users.rust-lang.org/t/how-to-return-bufreader/34651/6
+fn open_reader(fpath: &OsString) -> BufReader<Box<dyn Read>> {
+    let fileobj = Box::new(File::open(fpath).unwrap());
+    BufReader::new(fileobj)
+}
+
 fn main() {
     println!("Hello, world!");
+    let filepath = "C:\\RustRepo\\text_oxidizer\\test.txt".to_string();
+    println!("{}", filepath);
+    let nreader = open_reader(&OsString::from(filepath));
+    for line in nreader.lines() {
+        println!("{}", line.unwrap())
+    }
 }
