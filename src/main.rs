d3f73ea8823a194f4bec9ffc219593b5e53b9735
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-05-26 19:47:30 -0700

    Added error passthrough

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
 //https://users.rust-lang.org/t/how-to-return-bufreader/34651/6
-fn open_reader(fpath: &OsString) -> BufReader<Box<dyn Read>> {
-    let fileobj = Box::new(File::open(fpath).unwrap());
-    BufReader::new(fileobj)
+fn open_reader(fpath: &OsString) -> Result<BufReader<Box<dyn Read>>, std::io::Error> {
+    let fileobj = File::open(fpath)?;
+    Ok(BufReader::new(Box::new(fileobj)))
 }
 
 fn main() {
@@ -13,7 +13,12 @@ fn main() {
     let filepath = "C:\\RustRepo\\text_oxidizer\\test.txt".to_string();
     println!("{}", filepath);
     let nreader = open_reader(&OsString::from(filepath));
-    for line in nreader.lines() {
-        println!("{}", line.unwrap())
+
+    if let Ok(linereader) = nreader {
+        for line in linereader.lines() {
+            println!("{}", line.unwrap())
+        }
+    } else {
+        println!("Invalid filepath")
     }
 }
