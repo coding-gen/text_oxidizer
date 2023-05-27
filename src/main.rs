6c21b8292ccbf4063385dac74daefdbf6ee215d3
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-05-26 19:54:59 -0700

    Changed to use OsStr as arg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
 //https://users.rust-lang.org/t/how-to-return-bufreader/34651/6
-fn open_reader(fpath: &OsString) -> Result<BufReader<Box<dyn Read>>, std::io::Error> {
+fn open_reader(fpath: &OsStr) -> Result<BufReader<Box<dyn Read>>, std::io::Error> {
     let fileobj = File::open(fpath)?;
     Ok(BufReader::new(Box::new(fileobj)))
 }
@@ -12,7 +12,8 @@ fn main() {
     println!("Hello, world!");
     let filepath = "C:\\RustRepo\\text_oxidizer\\test.txt".to_string();
     println!("{}", filepath);
-    let nreader = open_reader(&OsString::from(filepath));
+    let ostringpath = OsString::from(filepath);
+    let nreader = open_reader(&ostringpath);
 
     if let Ok(linereader) = nreader {
         for line in linereader.lines() {
