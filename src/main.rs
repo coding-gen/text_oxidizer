856f6f5fc44be65786fb0470df1651594f423f99
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-05-26 20:09:00 -0700

    Moved test code into its own function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use std::ffi::{OsStr, OsString};
+use std::env;
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
 //https://users.rust-lang.org/t/how-to-return-bufreader/34651/6
+///Accepts a file path and returns a Result containing either a BufReader or an IO error
 fn open_reader(fpath: &OsStr) -> Result<BufReader<Box<dyn Read>>, std::io::Error> {
     let fileobj = File::open(fpath)?;
     Ok(BufReader::new(Box::new(fileobj)))
 }
 
 fn main() {
-    println!("Hello, world!");
-    let filepath = "C:\\RustRepo\\text_oxidizer\\test.txt".to_string();
-    println!("{}", filepath);
-    let ostringpath = OsString::from(filepath);
+    test_open_reader();
+}
+
+//Test functions.  May or may not be unit tests.
+
+//Assumes you have a test.txt in the current crate location
+fn test_open_reader() {
+    let mut filepath = env::current_dir().unwrap();
+    filepath.push("test.txt");
+    let ostringpath = filepath.into_os_string();
+    if let Ok(seepath) = ostringpath.clone().into_string() {
+        println!("{}", seepath);
+    } else {
+        println!("Filepath not displayable - continuing...")
+    }
     let nreader = open_reader(&ostringpath);
 
     if let Ok(linereader) = nreader {
