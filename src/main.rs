2ebe865430676bbc03ab7bc31f19125ae3d8a625
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-06-03 14:46:43 -0700

    Added struct to count matches and occurences.  Added function to process CSV and create HashMap of aforementioned struct.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use csv::Reader;
 use lazy_static::lazy_static;
 use prompted::input;
 use regex::Regex;
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::ffi::OsStr;
@@ -21,19 +22,54 @@ fn error(err: &str) -> ! {
     panic!("error");
 }
 
+//Struct for pairing a token stream to a target value for training models
 #[derive(Debug)]
 struct LineTarget {
     tokens: Vec<String>,
     target: String,
 }
 
+//Struct for pairing a number of target matches and token occurances for Bayes analysis
+#[derive(Debug)]
+struct TokenOccurence {
+    matches: usize,
+    occurences: usize,
+}
+
+fn create_bayes(input: Vec<LineTarget>, target: &str) -> HashMap<String, TokenOccurence> {
+    let mut bayes: HashMap<String, TokenOccurence> = HashMap::new();
+    for line in input {
+        for token in line.tokens {
+            if !bayes.contains_key(&token) {
+                bayes.insert(
+                    token.clone(),
+                    TokenOccurence {
+                        matches: 0,
+                        occurences: 0,
+                    },
+                );
+            }
+            let inplace = bayes.get_mut(&token).unwrap();
+            inplace.occurences += 1;
+            if line.target == target {
+                inplace.matches += 1;
+            }
+        }
+    }
+
+    bayes
+}
+
+//Accepts a path to a CSV file.  Just hands up any errors it recieves.
+//Otherwise returns a Vec of LineTargets for further processing.
 fn parse_csv_to_linetarget(fpath: &OsStr) -> Result<Vec<LineTarget>, Box<dyn Error>> {
     let mut out = Vec::new();
     let mut reader = Reader::from_path(fpath)?;
 
     for result in reader.byte_records() {
         let record = result?;
-        let tokens = tokenize_line_alphas(&String::from_utf8_lossy(record.get(1).unwrap()));
+        let tokens =
+            tokenize_line_alphas_lowercase(&String::from_utf8_lossy(record.get(1).unwrap()));
         let target = String::from_utf8_lossy(record.get(0).unwrap()).into_owned();
 
         let newout = LineTarget { tokens, target };
@@ -72,7 +108,7 @@ fn tokenize_line(line: &str) -> Vec<String> {
 //Using lazy_static as recommended by regex crate docs
 //Ignores invalid returns from the regex
 //Only returns alpha sequences, including apostraphies
-fn tokenize_line_alphas(line: &str) -> Vec<String> {
+fn tokenize_line_alphas_lowercase(line: &str) -> Vec<String> {
     let mut tokens = Vec::new();
     lazy_static! {
         static ref REGTOKEN: Regex = Regex::new(r#"[[:alpha:]']+"#).unwrap();
@@ -80,7 +116,7 @@ fn tokenize_line_alphas(line: &str) -> Vec<String> {
     for cap in REGTOKEN.captures_iter(line) {
         let text = &cap[0];
 
-        tokens.push(text.to_owned());
+        tokens.push(text.to_lowercase().to_owned());
     }
     tokens
 }
@@ -108,7 +144,8 @@ fn main() {
     // test_open_reader();
     // test_tokenize_line();
     // test_tokenize_reader();
-    test_parse_csv_to_linetarget();
+    // test_parse_csv_to_linetarget();
+    test_create_bayes();
 }
 
 //Test functions.  May or may not be unit tests.
@@ -202,5 +239,29 @@ fn test_parse_csv_to_linetarget() {
 
     for i in 0..8 {
         println!("{:?}", outvec.get(i))
+    }
+}
+
+fn test_create_bayes() {
+    let mut filepath = env::current_dir().unwrap();
+    let target = "not_relevant";
+    filepath.push("Twitter-sentiment-self-drive-DFE-Test.csv");
+
+    let ostringpath = filepath.into_os_string();
+    if let Ok(_seepath) = ostringpath.clone().into_string() {
+        //println!("{}", seepath);
+    } else {
+        println!("Filepath not displayable - continuing...")
+    }
+
+    let outvec = parse_csv_to_linetarget(&ostringpath).unwrap();
+    let bayes = create_bayes(outvec, target);
+
+    for line in bayes.iter() {
+        println!("Key: {}", line.0);
+        println!("TokenOccurence: {:?}", line.1);
+        if input!("End?") == "Q" {
+            std::process::exit(0);
+        }
     }
 }
