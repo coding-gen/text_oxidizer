db07e080b417ab5c44aa8bb586226bdd6de40494
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-06-11 20:35:36 -0700

    Started outlining the args parser

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,36 @@ mod tokenize;
 pub use crate::debug_tools::*;
 pub use crate::naive_bayes::*;
 pub use crate::tokenize::*;
+use clap::arg;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    ///NLP tool to use
+    #[arg(short, long)]
+    source: String,
+
+    ///Enable test mode - disregards filepath
+    #[arg(long, default_value_t = false)]
+    test: bool,
+
+    ///Use Naive Bayes.  Cannot be used with other NLP tools.
+    #[arg(long)]
+    naive_bayes: bool,
+
+    ///
+}
 
 fn main() {
     // test_naive_bayes_modeling();
     // test_naive_bayes_against_test();
-    println!("Stub")
+    let args = Args::parse();
+
+    println!("Argument: {}", args.tool);
+
+    if args.test {
+        println!("Test is enabled");
+    } else {
+        println!("Test is disabled");
+    }
 }
